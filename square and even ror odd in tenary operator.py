03c124164d5b756d193ccfe08ce4c974cb43cb74
234siri <sirikothasujatha@gmail.com>
2024-10-22 17:56:04 +0530

    Add files via upload

---
+++ square and even ror odd in tenary operator.py
@@ -0,0 +1,22 @@
+Python 3.9.8 (tags/v3.9.8:bb3fdcf, Nov  5 2021, 20:48:33) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = float(input("Enter the first number: "))
+Enter the first number: 6
+>>> num2 = float(input("Enter the second number: "))
+Enter the second number: 9
+>>> square1 = num1 ** 2
+>>> square2 = num2 ** 2
+>>> print(f"The square of {num1} is {square1}.")
+The square of 6.0 is 36.0.
+>>> print(f"The square of {num2} is {square2}.")
+The square of 9.0 is 81.0.
+>>> 
+>>> 
+>>> squares  of them
+SyntaxError: invalid syntax
+>>> num = int(input("Enter a number: "))
+Enter a number: 13
+>>> result = "Even" if num % 2 == 0 else "Odd"
+>>> print(f"The number {num} is {result}.")
+The number 13 is Odd.
+>>> 
