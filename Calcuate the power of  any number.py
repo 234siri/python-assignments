aae6729ac24b5cbc86dcdc8f5fa322daaa597a3f
234siri <sirikothasujatha@gmail.com>
2024-10-22 17:55:35 +0530

    Add files via upload

---
+++ Calcuate the power of  any number.py
@@ -0,0 +1,8 @@
+Python 3.9.8 (tags/v3.9.8:bb3fdcf, Nov  5 2021, 20:48:33) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> base = 4
+>>> exponent = 4
+>>> power = base ** exponent
+>>> print(f"{base} raised to the power of {exponent} is {power}.")
+4 raised to the power of 4 is 256.
+>>> 
